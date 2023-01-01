adc8801a7dcff2eb2be69d035b7bcece4875f855
Mikhail Nikolaev <cakenumber1@mail.ru>
2023-01-01 11:22:23 +0300

    impl transparency

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const paintCanvas = document.querySelector('.canvas');
 const { width, height } = paintCanvas;
 const context = paintCanvas.getContext( '2d' );
-context.fillStyle = "#252525";
-// context.fillStyle = "white";
+context.fillStyle = "rgba(0, 0, 0, 0)";
 context.fillRect(0, 0, width, height);
 
 
 let x = 0, y = 0;
 let isMouseOver = false;
-let clearInProcess = false
+let clearInProcess = false;
 
 const clear = (step) => {
-    let curStep = isNaN(step) ? 0 : step
-    const leftSteps = 10 - curStep
-    const imgData = context.getImageData( 0, 0, width, height)
+    let curStep = isNaN(step) ? 0 : step;
+    const leftSteps = 30 - curStep;
+    const imgData = context.getImageData( 0, 0, width, height);
 
     for (let i = 0; i < imgData.data.length / 4; i++) {
         const _i = i * 4;
+
         if (!leftSteps) {
-            imgData.data[_i]=37; //r
-            imgData.data[_i+1]=37; //g
-            imgData.data[_i+2]=37; //b
+            imgData.data[_i+1] = 0;
+            imgData.data[_i+3] = 0;
+        }
+        if (imgData.data[_i+3] < 255) {
+            imgData.data[_i+3] -= 33;
         } else {
-            if (imgData.data[_i] !== 37) {
-                imgData.data[_i] -= ((imgData.data[_i] - 37) / leftSteps);
-            }
-            if (imgData.data[_i+1] !== 37) {
-                imgData.data[_i+1] -= ((imgData.data[_i+1] - 37) / leftSteps);
-            }
-            if (imgData.data[_i+2] !== 37) {
-                imgData.data[_i+2] += ((37 -imgData.data[_i+2]) / leftSteps);
+            if (imgData.data[_i+1] < 45) {
+                imgData.data[_i+1] += 10;
+            } else if(imgData.data[_i+1] > 55) {
+                imgData.data[_i+1] = imgData.data[_i+1] > 70 ? imgData.data[_i+1] - 15 : 55;
+            } else if(imgData.data[_i+1] === 55) {
+                imgData.data[_i+1] = 60;
+                imgData.data[_i+3] -= 33;
+            } else {
+                imgData.data[_i+1] = 255;
             }
         }
     }
@@ -53,31 +56,27 @@ const draw = (x, y) => {
 
     for (let i = 0; i < imgData.data.length / 4; i++)
     {
-        const _x = i % side;
-        const _y = Math.trunc(i / side);
+        const _x = i % side - 10;
+        const _y = Math.trunc(i / side) - 10;
+
         // (x - center_x)² + (y - center_y)² < radius²
-        if ((_x - r)**2 + (_y - r)**2 < r**2) {
+        if ((_x)**2 + (_y)**2 < r**2) {
             const _i = i*4;
 
-            if (imgData.data[_i+2] == 37) {
-                imgData.data[_i]=255; //r
-                imgData.data[_i+1]=60; //g
-                imgData.data[_i+2]=0; //b
-                imgData.data[_i+3]=255;
-                // todo: transparency == white color for some reason
-                // imgData.data[_i+3]=255 - 255 * ((_x - r)**2 + (_y - r)**2) / r**2
+            if (imgData.data[_i+3] === 0) {
+                imgData.data[_i] = 255; //r
+                imgData.data[_i+1] = 60; //g
+                imgData.data[_i+2] = 0; //b
+                imgData.data[_i+3] = 125
+            } else  if (imgData.data[_i+3] < 255 ){
+                imgData.data[_i+3] += 44
             } else if (imgData.data[_i+1] > 55 && imgData.data[_i+1] !== 255) {
-                imgData.data[_i+1] += 10;
-                imgData.data[_i+3] = 255;
+                imgData.data[_i+1] += 15;
             } else if (imgData.data[_i+1] < 45) {
                 imgData.data[_i+1] = Math.min(imgData.data[_i+1] - 10, 0);
-                imgData.data[_i+3] = 255;
             } else {
                 imgData.data[_i+1] = 44;
-                imgData.data[_i+3] = 255;
             }
-            // alt to transparency
-            // imgData.data[_i+3]= ((_x - r/2)**2 + (_y - r/2)**2) < r**2 ? 0 : 255
         }
     }
     context.putImageData(imgData, x - r, y - r);
